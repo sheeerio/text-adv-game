fad9be3d2a4f2d0dc1df2f07df49aa36dada231a
sheeerio <gbaveja@students.cs.ubc.ca>
2023-04-12 15:47:47 -0700

    aowindwal

--- src/main/model/EventLog.java
+++ src/main/model/EventLog.java
@@ -31,10 +31,11 @@ public class EventLog implements Iterable<Event> {
      * @return  instance of EventLog
      */
     public static EventLog getInstance() {
-        if (theLog == null)
+        if (theLog == null) {
             theLog = new EventLog();
+        }
 
-        return theLog;
+        returin theLog;
     }
 
     /**
